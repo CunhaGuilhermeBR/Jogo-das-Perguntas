0456ac4804f21b5d7bcb288165247b403d5da1a1
Guilherme Augusto Gomes Cunha <41303580+CunhaGuilhermeBR@users.noreply.github.com>
2021-10-12 13:03:01 -0300

    Versão inicial

--- methods/actions.js
+++ methods/actions.js
@@ -3,13 +3,11 @@ const storage = require('node-sessionstorage')
 
 
 var functions = {
-    createQuestion: function(req, res){
-        console.log('errada1 ' + req.body.wrongAnswer1)
-        console.log('certa ' + req.body.rigthAnswer)
-        if(!req.body.difficulty || !req.body.area || !req.body.rigthAnswer || !req.body.wrongAnswer1 || !req.body.wrongAnswer2 || !req.body.wrongAnswer3)
-        res.render('menu', {error: true, msg: 'Preencha todos os campos.'})
-        else{
-            var newQuestion = Question ({
+    createQuestion: function (req, res) {
+        if (!req.body.difficulty || !req.body.area || !req.body.rigthAnswer || !req.body.wrongAnswer1 || !req.body.wrongAnswer2 || !req.body.wrongAnswer3)
+            res.render('menu', { error: true, msg: 'Preencha todos os campos.' })
+        else {
+            var newQuestion = Question({
                 area: req.body.area,
                 rigthAnswer: req.body.rigthAnswer,
                 wrongAnswer1: req.body.wrongAnwser1,
@@ -29,32 +27,54 @@ var functions = {
             })
         }
     },
-    makeQuestion: function (req,res){
-        Question.find({"area":req.body.area, "difficulty":req.body.difficulty}).lean().then((questions) => {
-            let quest= questions[Math.floor(Math.random()*questions.length)];
+    makeQuestion: function (req, res) {
+        Question.find({ "area": req.body.area, "difficulty": req.body.difficulty }).lean().then((questions) => {
+            let quest = questions[Math.floor(Math.random() * questions.length)];
             storage.setItem('rigthAnswer', quest.rigthAnswer)
+            storage.setItem('difficulty', quest.difficulty)
             var answers = shuffle([quest.rigthAnswer, quest.wrongAnswer1, quest.wrongAnswer2, quest.wrongAnswer3])
-            res.render('answer', {question: quest.question,  answers : answers })
-        }).catch((err)=>{
+            res.render('answer', { question: quest.question, answers: answers })
+        }).catch((err) => {
             console.log('erro ' + err)
             res.redirect('/')
         })
-       
     },
+    verifyAnswer: function (req, res) {
+        var answer = storage.getItem('rigthAnswer')
+        var score = storage.getItem('score')
+        var difficulty = storage.getItem('difficulty')
+        if (req.params.answer == answer) {
+            switch (difficulty) {
+                case 'Fácil':
+                    score += 1
+                    break;
+                case 'Médio':
+                    score += 2
+                    breal;
+                case 'Difícil':
+                    score += 3
+                    break;
+            }
+            storage.setItem('score', score)
+            res.render('game', {score: score, right: true, msg: 'Resposta certa' })
+        }else{
+            res.render('game', {score: score, wrong: true, msg: 'Resposta errada'})
+        }
+    }
 }
 
 
 // Função para embaralhar as respostas
 function shuffle(array) {
-    let currentIndex = array.length,  randomIndex;
+    let currentIndex = array.length, randomIndex;
 
     while (currentIndex != 0) {
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex--;
-      [array[currentIndex], array[randomIndex]] = [
-        array[randomIndex], array[currentIndex]];
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+        [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]];
     }
     return array;
-  }
+}
 
 module.exports = functions
--- routes/index.js
+++ routes/index.js
@@ -22,12 +22,16 @@ router.post('/getQuestion', actions.makeQuestion)
 //@desc Rota que redireciona para a tela do jogo e define a pontuação inicial como 0.
 //@route GET /
 router.get('/beginGame', (req,res) => {
- storage.setItem('Score', 0)
+ storage.setItem('score', 0)
  res.render('game',{score: 0})
 })
 
 //@desc Rota que faz o cadastro de uma pergunta no banco de dados
 //@route POST /
 router.post('/cadQuestion', actions.createQuestion)
+
+//@desc Rota que faz o cadastro de uma pergunta no banco de dados
+//@route POST /
+router.get('/verifyAnswer/:answer', actions.verifyAnswer)
 
 module.exports = router;
